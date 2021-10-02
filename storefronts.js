4ccc482ee758ae80b23ba69ed7c73ff0d0b445e2
Amir Andalibi <amir@pipemail.org>
2021-10-02 14:36:48 -0700

    List of storefronts

---
+++ storefronts.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    'code': 'ca',
+    'name': 'Canada'
+  },
+  {
+    'code': 'us',
+    'name': 'United States'
+  },
+  {
+    'code' : 'gb',
+    'name': 'United Kingdom'
+  },
+  {
+    'code': 'au',
+    'name': 'Australia'
+  },
+  {
+    'code': 'de',
+    'name': 'Germany'
+  },
+  {
+    'code': 'fr',
+    'name': 'France'
+  }
+];
